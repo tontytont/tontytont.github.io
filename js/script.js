2508da62425be0053d1da4a62598975f1cb8bb7f
Tony Wang <tontywang@gmail.com>
2017-09-24 20:22:21 -0400

    restrict to mobile

--- js/script.js
+++ js/script.js
@@ -244,16 +244,3 @@ function hidePreloader() {
 
 })//End
 
-
-var colors = ['#000','#fff'];
-var tmp = parseInt(Math.floor((Math.random()*2)));
-var color = colors[tmp];
-if(color==='#fff'){
-  $(".intro h1").css('color', color);
-  $(".routes a").css('color',color);
-  $("body").css('background-color','#000');
-  $("header").css('background-color','#000');
-} else {
-  $(".intro h1").css('color',color);
-  $(".routes a").css('color',color);
-}
